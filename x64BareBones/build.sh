c6fd5f5e47448631f5b4370c0225012977db2c09
matias <mholgado@itba.edu.ar>
2025-06-06 19:23:59 -0300

    Co-authored-by: Manuel Suñol <manuel-sunol@users.noreply.github.com>

--- x64BareBones/build.sh
+++ x64BareBones/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker start tpe-container
-docker exec -it tpe-container make clean -C /root/Toolchain
-docker exec -it tpe-container make clean -C /root/
-docker exec -it tpe-container make -C /root/Toolchain
-docker exec -it tpe-container make -C /root/
-docker stop tpe-container
+docker start tpe-arqui
+docker exec -it tpe-arqui make clean -C /root/Toolchain
+docker exec -it tpe-arqui make clean -C /root/
+docker exec -it tpe-arqui make -C /root/Toolchain
+docker exec -it tpe-arqui make -C /root/
+docker stop tpe-arqui
