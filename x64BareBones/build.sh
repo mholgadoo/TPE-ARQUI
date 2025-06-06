d711c5a0b348fcbd0ce8543b248a28adc1e0cffa
matias <mholgado@itba.edu.ar>
2025-06-06 19:50:42 -0300

    arreglo de build.sh  (no se por que en root se agrego x64BareBones)

--- x64BareBones/build.sh
+++ x64BareBones/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker start tpe-arqui
-docker exec -it tpe-arqui make clean -C /root/Toolchain
-docker exec -it tpe-arqui make clean -C /root/
-docker exec -it tpe-arqui make -C /root/Toolchain
-docker exec -it tpe-arqui make -C /root/
+docker exec -it tpe-arqui make clean -C /root/x64BareBones/Toolchain
+docker exec -it tpe-arqui make clean -C /root/x64BareBones
+docker exec -it tpe-arqui make -C /root/x64BareBones/Toolchain
+docker exec -it tpe-arqui make -C /root/x64BareBones
 docker stop tpe-arqui
